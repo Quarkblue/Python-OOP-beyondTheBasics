ebe7653e7afa5b399159a6c10136edb68f134b6d
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-06-26 03:48:37 +0530

    Create attributeEncapsulation.py

---
+++ Advanced Features/attributeEncapsulation.py
@@ -0,0 +1,43 @@
+# Attribute Encapsulation
+# Python beyond the basics - object oriented programming
+
+"""
+Instance attributes can be changed anywhere, to any value,
+breaking encapsulation.
+Getter and Setter methods encourage encapsulation, but are
+clunkier.
+Users are expected to "do the right thing".
+Trusting the user is pythonic.
+
+@ property is a decorator that designates an instance attribute
+as encapsulate-able through methods.
+Getting,setting and deleting methods can be defined, called
+automatically when a user accesses the attribute.
+The names are linked to the attribute name - the methods, and
+setter and deleter methods, must use it.
+@property offers some control, but don't try to force the user
+into certain behaviours - it's un-Pythonic.
+"""
+
+
+class GetSet(object):
+    
+    def __init__(self,value):
+        self.attrval = value
+        
+    @property
+    def var(self):
+        print("Getting the var attribute")
+        return self.attrval
+    
+    @var.setter
+    def var(self,value):
+        print("Setting the var attribute")
+        self.attrval = value
+        
+    @var.deleter
+    def var(self):
+        print("Deleting the vat attribute")
+        self.attrval = None
+        
+        
