88d63bdeb1bcd679c6304e3755e69be12bc1318d
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-06-29 15:22:09 +0530

    .

---
+++ Exceptions/trappingAndRaisingExcep.py
@@ -0,0 +1,12 @@
+# Trapping and Raising Exceptions
+# Python Beyond the basics - Object oriented programming
+
+"""
+Exceptions and exceptions classes
+
+Identifying exceptions(errors).
+Trapping exceptions with try/except.
+Raising exception with raise.
+Defining custom exception classes.
+"""
+
--- test.py
+++
@@ -1,8 +0,0 @@
-import sys
-from Assignments.Assignment3 import ConfigDict
-
-cd = ConfigDict('config_file.txt')
-
-key = "hell"
-value = "h"
-cd[key] = value
