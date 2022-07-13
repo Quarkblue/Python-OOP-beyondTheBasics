4f9b167ce1713f76dae9a70dd75e406dfe06b1da
Divyansh Chauhan(Quark Blue) <d.s.c.r.3012@gmail.com>
2022-07-13 19:41:13 +0530

    .

--- random c codes/test.c
+++ random c codes/test.c
@@ -3,14 +3,17 @@
 int main(){
 
    int n;
+
    scanf("%d", &n);
 
    int fact,i;
    fact=1;
 
-   for(i=1;i<=n;i++){
+   for(int i=1;i<=n;i++){
       fact=fact*i;
    }
    printf("%d",fact);
+
+   return 0;
 
 }
